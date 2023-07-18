e416bd7373691a5d60413394763b3c34ad4ddf0d
melindamackenzie <mem167@pitt.edu>
2023-07-17 20:25:45 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+let num3 = 12
+let num4 = 3
+let mod = (num3 / num4)
+let array = [5, 10, 15, 20]
+let max = Math.max(20)
+let random = parseInt((Math.random() * 100) + 1)
